80eac468f1955fbad47785ba949683443f16c596
Christian Scheb <privat@stylemotion.de>
2019-09-16 21:30:18 +0200

    Change sampling rate

--- beatDetector.py
+++ beatDetector.py
@@ -10,7 +10,7 @@ class BeatDetector:
     ui: ui.UserInterface
     osc_client: osc.OscClient
     input_recorder: InputRecorder
-    timer_period = 16.66666
+    timer_period = 1000 / (180 / 60) / 32  # 180bpm / 8
     bar_modulo = 8
 
     def __init__(self, window) -> None:
